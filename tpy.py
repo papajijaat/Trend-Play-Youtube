7c10720c19717908407e7a1f5fde732ae13dd868
Anmol Singh <theanmolmangat@gmail.com>
2016-12-30 03:34:09 +0530

    Update tpy.py

--- tpy.py
+++ tpy.py
@@ -1,4 +1,4 @@
-# Play trending Videos
+#Play Trending Videos
 
 from bs4 import BeautifulSoup
 import requests
@@ -7,23 +7,34 @@ import subprocess
 
 response = requests.get("https://www.youtube.com/feed/trending")
 
-data = response.txt
-soup = BeautifulSoup(data,"lxml")
+data = response.text
+soup = BeautifulSoup(data,"lxml") 
 
-#get all the header tags
+#get all header tags
 
-i=0;
-con = TRUE
-while con == TRUE:
-       l1=[]
-       for link in soup.find_all("h3",{"class":"yt-lockup-title"}):
-            for var in link.find_all("a"):
-                    title =  var.get("title")
-                    href = var.get("href")
-                    print("Title[%s] = %s\n"%(i,title))
-                    l1.insert(i,href)
-                    i=i+1
-                    
-       choice = int(raw_input("Enter your choice of video or press -1"))
-       select = l1[choice]
-       link = "https://www.youtube.com"+select
+i=0 
+con = True
+while con == True:
+      list1=[]
+      for link in soup.find_all("h3",{"class":"yt-lockup-title"}):
+          for var in link.find_all("a"):
+              title = var.get("title")
+              href = var.get("href")
+              print("Title[%s] = %s\n"%(i,title))
+              list1.insert(i,href)
+              i=i+1
+
+      choice = int(raw_input("Enter your choice of video or press -1 "))
+      select =list1[choice]
+      link = "https://www.youtube.com"+select
+      
+      if choice!=-1:
+         option = raw_input("VLC(v) or Youtube(y) ")
+         if option=='y':
+            webbrowser.open(link)
+         else:
+            myprocess = subprocess.call(['vlc','-vvv',link])
+      choice=raw_input("Want to continue y/n ")
+      if choice=='n':
+         con=False
+      i=0   
